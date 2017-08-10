4724d5e4a5a33efd83837e3606b3afb775957f0b
preventabully <preventabully@gmail.com>
2017-08-09 22:47:53 -0400

    sip

--- form2.js
+++ form2.js
@@ -52,15 +52,15 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     var url = event.target.action;  //
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
-    // xhr.withCredentials = true;
+    xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
         document.getElementById('gform').style.display = 'none'; // hide form
         document.getElementById('thankyou_message').style.display = 'block';
-        return;
-    };
+        return
+     };
     // url encode form data for sending as post data
     var encoded = Object.keys(data).map(function(k) {
         return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
@@ -68,6 +68,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.send(encoded);
   }
 }
+
 function loaded() {
   console.log('contact form submission handler loaded successfully');
   // bind to the submit event of our form
